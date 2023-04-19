94930ce5feaf6667f4ea34a7d27a13c65bbd1314
Billy Joe Hamphries <woistmeinbier@yahoo.com>
2023-04-19 22:10:18 +0200

    Upload project

---
+++ IPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseChallenge
+{
+    internal interface IPage
+    {
+        void Page();
+    }
+}
